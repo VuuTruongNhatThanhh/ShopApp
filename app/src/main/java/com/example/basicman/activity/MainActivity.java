165a0146e357b2a10c49a13a8c8fa432ac0cca00
17130140 <66461780+17130140@users.noreply.github.com>
2023-05-22 20:43:40 +0700

    Add files via upload

--- app/src/main/java/com/example/basicman/activity/MainActivity.java
+++ app/src/main/java/com/example/basicman/activity/MainActivity.java
@@ -1,162 +1,89 @@
-package com.example.basicman.activity;
+package com.example.appbanhang.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
 
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.widget.Toolbar;
-
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.app.Notification;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.FrameLayout;
 
-import android.widget.Toast;
-import android.widget.ViewFlipper;
-
-import com.bumptech.glide.Glide;
-import com.example.basicman.R;
-import com.example.basicman.adapter.TypeProductAdapter;
-import com.example.basicman.model.TypeProduct;
-import com.example.basicman.model.TypeProductModel;
-import com.example.basicman.retrofit.ApiShop;
-import com.example.basicman.retrofit.RetrofitClient;
-import com.example.basicman.utils.Utils;
-import com.google.android.material.navigation.NavigationView;
-
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.List;
-
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
-
-
-
+import com.example.appbanhang.R;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     ViewFlipper viewFlipper;
-    RecyclerView recyclerViewHome;
+    RecyclerView recyclerViewManHinhChinh;
     NavigationView navigationView;
-    ListView listViewHome;
+    ListView listViewManHinhChinh;
     DrawerLayout drawerLayout;
-    TypeProductAdapter typeProductAdapter;
-    List<TypeProduct> arrayTypeProduct;
+    LoaiSpAdaptern loaiSpAdapter;
+    List<LoaiSp> mangloaisp;
     CompositeDisposable compositeDisposable = new CompositeDisposable();
-    ApiShop apiShop;
-    TypeProductModel typeProductModel;
-
-
-
+    ApiBanHangapiBanHang;
+    List<SanPhamMoi> mangSpMoi;
+    SanPhamMoiAdapter spAdapter;
+    NotificationBadge badge;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        apiShop = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiShop.class);
-        Anhxa();
-        ActionBar();
-//        ActionViewFlipper();
-        if(isConnected(this)){
-            ActionViewFlipper();
-            Toast.makeText(getApplicationContext(),"ok",Toast.LENGTH_LONG).show();
-            getTypeProduct();
-        }else{
-            Toast.makeText(getApplicationContext(),"not internet",Toast.LENGTH_LONG).show();
-        }
-        
-    }
-
-    private void getTypeProduct() {
-        compositeDisposable.add(apiShop.getTypeProduct()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        typeProductModel ->{
-                            if(typeProductModel.isSuccess()){
-                                arrayTypeProduct = typeProductModel.getResult();
-                                //set adapter
-                                typeProductAdapter = new TypeProductAdapter(getApplicationContext(),arrayTypeProduct);
-                                listViewHome.setAdapter(typeProductAdapter);
-
-                            }
-                        }
-                ));
-
-
-
-    }
-
-    private void ActionViewFlipper() {
-        List<String> arrayAdvertise = new ArrayList<>();
-        arrayAdvertise.add("https://lh6.googleusercontent.com/igI1pOYn1EhPkm8WaJb6TmGGP2Ya6cEztplx2jAhnl-y54VP2ZsHEbUl4zRIGNv2Nc52K03U4Sl0Pwq60o3ROxeqn9xYsaXm6Z4ToioTDDc6oeBUPRQLTw-VqVIDKr5QPNbwsLlD");
-        arrayAdvertise.add("https://top10tphcm.com/wp-content/uploads/2018/11/1-1024x819-Copy.jpg");
-        arrayAdvertise.add("https://mauthietkecuahang.com/wp-content/uploads/2019/04/huong-dan-set-up-shop-thoi-trang-nam-60m2-chuyen-nghiep-2-800x513.jpg");
-        for(int i=0; i<arrayAdvertise.size(); i++){
-            ImageView imageView = new ImageView(getApplicationContext());
-            Glide.with(getApplicationContext()).load(arrayAdvertise.get(i)).into(imageView);
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            viewFlipper.addView(imageView);
-
-        }
-        viewFlipper.setFlipInterval(3000);
-        viewFlipper.setAutoStart(true);
-        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
-        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
-        viewFlipper.setInAnimation(slide_in);
-        viewFlipper.setOutAnimation(slide_out);
-
-
     }
 
     private void ActionBar() {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        toolbar.setNavigationOnClickListener(new View.OnCLickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
     }
 
     private void Anhxa() {
-        toolbar = findViewById(R.id.toolbarhome);
-//        setSupportActionBar(toolbar);
-
-        viewFlipper = findViewById(R.id.viewflipper);
-        recyclerViewHome = findViewById(R.id.recycleview);
-        listViewHome = findViewById(R.id.listviewhome);
+        toolbar = findViewById(R.id.toobarmanhinhchinh);
+        viewFlipper = findViewById(R.id.viewlipper);
+        recyclerViewManHinhChinh = findViewById(R.id.recycleview);
+        RecyclerView.LayoutManager LayoutManager = new GridLayoutManager(this, 2);
+        recyclerViewManHinhChinh.setLayoutManager(LayoutManager);
+        recyclerViewManHinhChinh.setHasFixedSize(true);
+        ListViewManHinhChinh = findViewById(R.id.Listviewmanhinhchinh);
         navigationView = findViewById(R.id.navigationview);
         drawerLayout = findViewById(R.id.drawerlayout);
-        //set list
-        arrayTypeProduct = new ArrayList<>();
+        badge = findViewById(R.id.menu_sl);
+        frameLayout= findViewById(R.id.framegiohang);
+        //khoi tao list
+        mangloaisp = new ArrayList<>();
+        mangSpMoi = new ArrayList<>();
+        if (Utils.manggiohang == null) {
+            Utils.manggiohang = new ArrayList<>();
 
-    }
-    private boolean isConnected(Context context){
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        if((wifi!=null && wifi.isConnected())||(mobile!=null && mobile.isConnected())){
-            return true;
         }else{
-            return false;
+            int totalItem = 0;
+            for (int i=0; i<Utils.manggiohang.size(); i++) {
+                totalItem = totalItem + Utils.manggiohang.get(i).getSoluong();
+            }
+            badge,setText (String. valueOf (totalItem));
+
         }
+        frameLayout.setOnClickListener (new View.OnCLickListener() }
+        @Override
+        public void onClick(View view) {
+            Intent giohang = new Intent (getApplicationContext(), GioHangActivity.class);
+            startActivity(giohang);
 
 
     }
-    public void onDestroy(){
-        compositeDisposable.clear();
-        super.onDestroy();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int totalItem = 0;
+        for (int i = 0; i < Utils.manggiohang.size(); i++) {
+            totalItem = totalItem + Utils.manggiohang.get(i).getSoluong();
+        }
+        badge, setText(String.valueOf(totalItem));
+
     }
 }
