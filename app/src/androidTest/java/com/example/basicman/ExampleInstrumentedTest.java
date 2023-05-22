53dd67ec033295e9eb3f164f2f52f68b6995bdc2
17130140 <66461780+17130140@users.noreply.github.com>
2023-05-22 20:17:33 +0700

    Add files via upload

--- app/src/androidTest/java/com/example/basicman/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/basicman/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.basicman;
+package com.example.appbanhang;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.basicman", appContext.getPackageName());
+        assertEquals("com.example.appbanhang", appContext.getPackageName());
     }
 }
