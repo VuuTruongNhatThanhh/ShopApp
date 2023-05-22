71314820f0b9c612b0111b5a2005ada35c7ed7ab
17130140 <66461780+17130140@users.noreply.github.com>
2023-05-22 23:06:29 +0700

    Add files via upload

--- app/src/main/java/com/example/basicman/utils/Utils.java
+++ app/src/main/java/com/example/basicman/utils/Utils.java
@@ -1,5 +1,7 @@
-package com.example.basicman.utils;
-
+package com.example.appbanhang.Utils;
+import java.util.List;
+import com.example.appbanhang.model.GioHang;
 public class Utils {
-    public static final String BASE_URL ="http://192.168.0.104/basicman/";
+    public static final String BASE_URL="http://192.168.0.105/banhang/";
+    public static List<GioHang> manggiohang;
 }
